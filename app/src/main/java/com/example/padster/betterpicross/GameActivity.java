1f939dcde005f96174056f68bd2a77258311ce49
theRealPadster <padspambox@gmail.com>
2017-09-28 11:23:35 -0400

    Create button grid on GameActivity onCreate , Clean up MainActivity click listeners, Add landscape layout for GenerateActivity

--- app/src/main/java/com/example/padster/betterpicross/GameActivity.java
+++ app/src/main/java/com/example/padster/betterpicross/GameActivity.java
@@ -1,7 +1,13 @@
 package com.example.padster.betterpicross;
 
+import android.content.Context;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
@@ -16,11 +22,32 @@ public class GameActivity extends AppCompatActivity {
         int rows = bundle.getInt("rows");
         int cols = bundle.getInt("cols");
 
-        TextView rowsTextView = (TextView) findViewById(R.id.rows);
-        TextView colsTextView = (TextView) findViewById(R.id.cols);
+        //////////////////////////
 
-        rowsTextView.setText(Integer.toString(rows));
-        colsTextView.setText(Integer.toString(cols));
+        // get a reference for the TableLayout
+        TableLayout table = (TableLayout) findViewById(R.id.gameTableLayout);
+
+        for (int rowCounter = 0; rowCounter < rows; rowCounter++) {
+
+            // create a new TableRow
+            TableRow row = new TableRow(this);
+
+            for (int colCounter = 0; colCounter < cols; colCounter++) {
+
+                //create new button
+                Button b = new Button(this);
+                b.setLayoutParams(new TableRow.LayoutParams(0,TableRow.LayoutParams.WRAP_CONTENT));
+                b.setText(colCounter + "," + rowCounter);
+
+                // add the button to the new TableRow
+                row.addView(b);
+            }
+
+            // add the TableRow to the TableLayout
+            table.addView(row,new TableLayout.LayoutParams(TableLayout.LayoutParams.WRAP_CONTENT, TableLayout.LayoutParams.WRAP_CONTENT));
+
+        }
+
 
     }
 }
--- app/src/main/java/com/example/padster/betterpicross/MainActivity.java
+++ app/src/main/java/com/example/padster/betterpicross/MainActivity.java
@@ -4,27 +4,39 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button aboutBtn = (Button) findViewById(R.id.aboutBtn);
+        Button tutorialBtn = (Button) findViewById(R.id.tutorialBtn);
+        Button generateBtn = (Button) findViewById(R.id.generateBtn);
+        aboutBtn.setOnClickListener(this);
+        tutorialBtn.setOnClickListener(this);
+        generateBtn.setOnClickListener(this);
     }
 
-    public void aboutClicked(View v) {
-        Intent intent = new Intent(this, AboutActivity.class);
-        startActivity(intent);
-    }
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch (v.getId()) {
+            case R.id.aboutBtn:
+                intent = new Intent(this, AboutActivity.class);
+                break;
+            case R.id.tutorialBtn:
+                intent = new Intent(this, TutorialActivity.class);
+                break;
+            case R.id.generateBtn:
+                intent = new Intent(this, GenerateActivity.class);
+                break;
+        }
 
-    public void tutorialClicked(View v) {
-        Intent intent = new Intent(this, TutorialActivity.class);
-        startActivity(intent);
-    }
-
-    public void generateClicked(View v) {
-        Intent intent = new Intent(this, GenerateActivity.class);
         startActivity(intent);
     }
 }
