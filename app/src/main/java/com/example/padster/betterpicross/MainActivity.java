7bd2109be8a02d919cbdca9a8864c7ad6b466cf4
theRealPadster <padspambox@gmail.com>
2017-09-28 12:14:51 -0400

    Show the tutorial once on first app launch

--- app/src/main/java/com/example/padster/betterpicross/MainActivity.java
+++ app/src/main/java/com/example/padster/betterpicross/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.padster.betterpicross;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -20,6 +22,46 @@ public class MainActivity extends AppCompatActivity implements OnClickListener {
         aboutBtn.setOnClickListener(this);
         tutorialBtn.setOnClickListener(this);
         generateBtn.setOnClickListener(this);
+
+
+        //  Declare a new thread to do a preference check
+        Thread t = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                //  Initialize SharedPreferences
+                SharedPreferences getPrefs = PreferenceManager
+                        .getDefaultSharedPreferences(getBaseContext());
+
+                //  Create a new boolean and preference and set it to true
+                boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
+
+                //  If the activity has never started before...
+                if (isFirstStart) {
+
+                    //  Launch app intro
+                    final Intent i = new Intent(MainActivity.this, TutorialActivity.class);
+
+                    runOnUiThread(new Runnable() {
+                        @Override public void run() {
+                            startActivity(i);
+                        }
+                    });
+
+                    //  Make a new preferences editor
+                    SharedPreferences.Editor e = getPrefs.edit();
+
+                    //  Edit preference to make it false because we don't want this to run again
+                    e.putBoolean("firstStart", false);
+
+                    //  Apply changes
+                    e.apply();
+                }
+            }
+        });
+
+        // Start the thread
+        t.start();
+
     }
 
     @Override
